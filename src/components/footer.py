d2201537d5194763e6ca2a529f64f3f40672c2fd
Pratyush Hirawat <154426448+Pratyushhirawat@users.noreply.github.com>
2026-06-10 19:58:02 +0530

    pipelines created

--- src/components/footer.py
+++ src/components/footer.py
@@ -11,3 +11,14 @@ def footer_home():
         </div>   
                 
                 """, unsafe_allow_html=True)
+    
+def footer_dashboard():
+
+   
+    
+    st.markdown(f"""
+         <div style="margin-top:2rem; display:flex; gap:6px; justify-content:center; items-align:center">
+            <p style="font-weight:bold; color:black;">Created with 💗 by Pratyush Hirawat</p>
+        </div>   
+                
+                """, unsafe_allow_html=True)
